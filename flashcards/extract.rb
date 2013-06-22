5e79f2aebe0c4b95117a9ebece9fa3bbd7cc3f82
Fred Brockstedt <Fred.Brockstedt@BeLUG.de>
2013-06-22 17:37:17 +0200

    automatisches finden von Namen fuer flashcards

--- flashcards/extract.rb
+++ flashcards/extract.rb
@@ -3,18 +3,39 @@
 # Fred Brockstedt, HU-Berlin, 2013
 
 # Dieses Programm erstellt 'flashcards' aus einem LaTeX Skript
+# Beispiel: ruby extract.rb ../Stochastik_Skript >> flashcards.tex
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+################################################################################
 # Variablen
 record = FALSE
 texenvs  = ["defn", "prop", "prop*", "lem", "lem*"]
 
+skript = ARGV[0]
+
+fcname = "" # Name fuer die Flashcard
+fcrseite = "" # Aufschrift der Rueckseite
+fctyp = {"defn" => "Definition", 
+"prop"  => "Satz", 
+"prop*" => "Satz", 
+"lem"   => "Lemma", 
+"lem*"  => "Lemma"
+}
+
+################################################################################
+# Main
 for texenv in texenvs # Fuer alle Umgebungen (texenv)
-  File.open('../Stochastik_Skript.tex').each do |line|  # Fuer alle Zeilen im Skript
+  File.open(skript).each do |line|  # Fuer alle Zeilen im Skript
     if( line =~ /\\begin\{#{texenv}\}/)     # startet mit \begin{defn}
       record = TRUE # ab hier die folgenden Zeilen in die flashcard aufnehmen
       puts "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-      puts "\\begin{flashcard}[#{texenv}]{???}"
+      fcname = line.scan(/\\begin\{#{texenv}\}\[([^\]].*)\]/) # Name extrahieren
+      if(fcname == []) # falls kein Name vorhanden
+        fcname = line.scan(/\% flashcard-name: (.*)$/) # Name per Kommentar finden
+      else
+        fcname = fcname.last.first
+      end
+
+      puts "\\begin{flashcard}[#{fctyp[texenv]}]{#{fcname}}"
       next # diese Zeile enthaelt \begin{...} also ueberspringen 
     end
 
